5e9292bea3e96887e355c874cb2ec996dc6e893d
Leandro Machado Costa <leandro.275@gmail.com>
2015-04-17 10:13:28 -0300

    Add http://leandro.js.org/
    Create new Subdomain leandro.js.org

--- cnames_active.js
+++ cnames_active.js
@@ -86,4 +86,5 @@ var cnames_active = {
     , "xto6": "mohebifar.github.io/xto6"
     , "zodiac": "indus.github.io/Zodiac"
     , "zombie": "assaf.github.io/zombie"
+    , "leandro": "lmc275.github.io/leandro/"
 }
